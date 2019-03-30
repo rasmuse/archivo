edc597dda15b356435f11d77e0b026246b64c529
Rasmus Einarsson <mr@rasmuseinarsson.se>
2019-03-30 13:47:11 +0100

    Use attrs

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    'Click>=7.0',
+    'attrs>=19.1.0',
+    ]
 
 setup_requirements = ['pytest-runner', ]
 
