7b4c2598e0522ae3f8f8545d137da8b1b63dfebe
Rasmus Einarsson <mr@rasmuseinarsson.se>
2019-03-31 15:48:21 +0200

    Go back to tree representation
    Traversing is a little more fuss but it's good because
    (1) it simplifies other parts of the code, and
    (2) it clarifies and enforces the requirement that a dataset is
        exactly one file or directory

--- archivo/archivo.py
+++ archivo/archivo.py
@@ -2,6 +2,7 @@
 
 """Main module."""
 
+from __future__ import annotations
 from pathlib import Path
 import os
 import json
@@ -18,149 +19,88 @@ from typing import (
     Generator,
     Tuple,
     Any,
+    TypeVar,
+    NewType,
+    Callable,
     )
 import attr
 
-DEFAULT_HASH = 'sha256'
-
-
-
-def _get_stat_data(file):
-    stat_result = os.stat(file.fileno())
-    return {
-        'mode': stat_result.st_mode,
-        'size': stat_result.st_size,
-        'ctime_ns': stat_result.st_ctime_ns,
-        'atime_ns': stat_result.st_atime_ns,
-        'mtime_ns': stat_result.st_mtime_ns,
-    }
-
-def _restore_stat_data(path, file_spec):
-    os.chmod(path, file_spec.mode)
-    os.utime(path, ns=(file_spec.atime_ns, file_spec.mtime_ns))
-
-
-_EQUIV_ATTRIBUTES = [
-    'hash_name',
-    'hexdigest',
-    'mode',
-    'mtime_ns',
-    'size',
-]
-
-@attr.s(auto_attribs=True)
-class FileSpec:
-    hash_name: str
-    hexdigest: str
-    mode: int
-    atime_ns: int
-    mtime_ns: int
-    size: int
-    ctime_ns: int
-
-
-    def _equiv_dict(self):
-        return {k: getattr(self, k) for k in _EQUIV_ATTRIBUTES}
-
-    def is_equivalent(self, other):
-        return self._equiv_dict() == other._equiv_dict()
-
-FileList = Dict[Path, FileSpec]
-
-
-def write_json_file(data, path):
-    with open(path, 'w') as f:
-        json.dump(data, f, indent=2)
-        f.write('\n')
-
-def now_to_text():
-    return datetime.datetime.utcnow().isoformat()
-
-def datetime_from_text(s):
-    return datetime.datetime.fromisoformat(s)
+T = TypeVar('T')
 
 path_like = Union[Path, str]
 
-def _to_abs_path(path_like: path_like) -> Path:
-    return Path(path_like).resolve()
+DEFAULT_HASH = 'sha256'
 
-def format_datetime(dt, repository):
-    return dt.strftime(repository.datetime_format)
+def read_file_meta(path):
+    stat_result = os.stat(path)
+    return {
+        'mtime_ns': stat_result.st_mtime_ns,
+        'atime_ns': stat_result.st_atime_ns,
+        'mode': stat_result.st_mode,
+        'size': stat_result.st_size,
+    }
 
+def set_mode(path, mode):
+    os.chmod(path, mode)
 
-_CHUNK_SIZE = 8096
+def set_mtime_ns(path, mtime_ns):
+    atime_ns = read_file_meta(path)['atime_ns']
+    os.utime(path, ns=(atime_ns, mtime_ns))
 
-def make_file_spec(path: Path, hash_name: str) -> FileSpec:
-    if not path.is_file():
-        raise ValueError(f'the path {path} is not a file')
+_CHUNK_SIZE = 4096
 
+def get_file_hexdigest(path: path_like, hash_name: str) -> str:
     m = hashlib.new(hash_name)
     with open(path, 'rb') as f:
-        stat_data = _get_stat_data(f)
         while True:
             data = f.read(_CHUNK_SIZE)
             if not data:
                 break
             m.update(data)
+    return m.hexdigest()
 
-    return FileSpec(hash_name, m.hexdigest(), **stat_data)
+@attr.s(auto_attribs=True)
+class FileSpec:
+    name: str
+    hash_name: str
+    hexdigest: str
+    mode: int
+    mtime_ns: int
+    size: int
 
-
-def _generate_relative_file_paths(
-    current_dir: Path,
-    root_dir=None
-    ) -> Generator[Path, None, None]:
-
-    assert current_dir.is_dir(), current_dir
-    assert current_dir.is_absolute()
-
-    if not root_dir:
-        root_dir = current_dir
-
-    for child in current_dir.iterdir():
-        assert child.is_absolute()
-        if child.is_file():
-            yield child.relative_to(root_dir)
-        else:
-            yield from _generate_relative_file_paths(child, root_dir=root_dir)
-
-def make_file_list(the_dir: Path, hash_name: str) -> FileList:
-    if not the_dir.is_dir():
-        raise ValueError(f'the path {the_dir} is not a directory')
-
-    files = {
-        child: make_file_spec(child, hash_name)
-        for child in _generate_relative_file_paths(the_dir)
-        }
-
-    return files
+@attr.s(auto_attribs=True)
+class DirSpec:
+    name: str
+    hash_name: str
+    contents: Sequence[FileOrDirSpec]
+    mode: int
+    mtime_ns: int
 
 
-def _move_files(moves):
-    for src_path, dst_path in moves.items():
-        os.makedirs(dst_path.parent, exist_ok=True)
-        os.rename(src_path, dst_path)
+FileOrDirSpec = Union[FileSpec, DirSpec]
+
+def _iter_paths_and_specs(spec, subdir, **kwargs):
+    this_path = ensure_rel(subdir / spec.name)
+    if isinstance(spec, FileSpec):
+        if kwargs['files']:
+            yield (this_path, spec)
+    else:
+        if kwargs['dirs']:
+            yield (this_path, spec)
+        for child in spec.contents:
+            yield from _iter_paths_and_specs(child, this_path, **kwargs)
 
 
-class CollisionError(Exception):
-    pass
+def iter_paths_and_specs(
+    spec: FileOrDirSpec,
+    dirs=True,
+    files=True,
+    ) -> Generator[Tuple[RelPath, FileSpec]]:
+    start_dir = Path('.')
+    return _iter_paths_and_specs(spec, start_dir, dirs=dirs, files=files)
 
 
-class RestoreError(Exception):
-    pass
-
-
-def _get_storage_meta_path(storage_dir: Path) -> Path:
-    return storage_dir / '.archivo-storage'
-
-
-def _validate_storage(instance, attribute, value):
-    storage_meta_path = _get_storage_meta_path(value)
-    if not storage_meta_path.is_file():
-        raise ValueError(f'metadata file {storage_meta_path} not found')
-
-
-def _get_apparent_name(path: Path) -> str:
+def get_apparent_name(path: Path) -> str:
     # Doing path.resolve() on a symlink would give the name of what
     # the symlink points to, but we want the name of the symlink
     # to represent the file or directory of what the symlink points to.
@@ -173,9 +113,82 @@ def _get_apparent_name(path: Path) -> str:
         return path.resolve().name
 
 
-def _copy_into(src_path: Path, dst_dir: Path) -> None:
-    name = _get_apparent_name(src_path)
-    dst_path = dst_dir.joinpath(name)
+def read_spec(path: path_like, hash_name: str) -> FileOrDirSpec:
+    path = Path(path)
+    # Doing stat first of all to get it before any possible modification
+    # by following operations
+    file_meta = read_file_meta(path)
+
+    if path.is_dir():
+        type_ = DirSpec
+    elif path.is_file():
+        type_ = FileSpec
+    else:
+        raise ValueError(f'path {path} has unsupported type')
+
+    kwargs = {}
+    kwargs['name'] = get_apparent_name(path)
+    kwargs['hash_name'] = hash_name
+    kwargs['mode'] = file_meta['mode']
+    kwargs['mtime_ns'] = file_meta['mtime_ns']
+
+    if type_ == FileSpec:
+        kwargs['hexdigest'] = get_file_hexdigest(path, hash_name)
+        kwargs['size'] = file_meta['size']
+
+    if type_ == DirSpec:
+        kwargs['contents'] = [
+            read_spec(child, hash_name)
+            for child in path.iterdir()
+            ]
+
+    return type_(**kwargs)
+
+
+AbsPath = NewType('AbsPath', Path)
+RelPath = NewType('RelPath', Path)
+
+def ensure_abs(path: path_like) -> AbsPath:
+    resolved = Path(path).resolve()
+    return AbsPath(resolved)
+
+def ensure_rel(path: path_like) -> RelPath:
+    path = Path(path)
+    if path.is_absolute():
+        raise ValueError(f'path {path} is not relative')
+    return RelPath(path)
+
+def write_json_file(data, path):
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=2)
+        f.write('\n')
+
+def now_to_text():
+    return datetime.datetime.utcnow().isoformat()
+
+
+class CollisionError(Exception):
+    def __init__(self, message, desired, existing):
+        self.message = message
+        self.desired = desired
+        self.existing = existing
+
+    def __str__(self):
+        return self.message
+
+class RestoreError(Exception):
+    def __init__(self, message, desired, restored):
+        self.message = message
+        self.desired = desired
+        self.restored = restored
+
+    def __str__(self):
+        return self.message
+
+
+def _copy_into(src_path: Path, dst_dir: Path) -> Path:
+    name = get_apparent_name(src_path)
+    dst_path = dst_dir / name
 
     if src_path.is_dir():
         shutil.copytree(src_path, dst_path)
@@ -184,101 +197,111 @@ def _copy_into(src_path: Path, dst_dir: Path) -> None:
     else:
         raise ValueError(f'path {src_path} is of unsupported type')
 
+    return dst_path
+
 
 @attr.s(auto_attribs=True)
 class Storage:
-    path: Path = attr.ib(converter=_to_abs_path, validator=_validate_storage)
+    path: AbsPath = attr.ib(converter=ensure_abs)
     meta: Dict[str, Any] = attr.ib(init=False, repr=False)
     hash_name: str = attr.ib(init=False, repr=False)
 
-    def get_file_path(self, file_spec):
+    def _get_storage_path(self, file_spec):
         return self.path / f'{file_spec.hash_name}/{file_spec.hexdigest}'
 
-    def get_tmp_dir(self):
-        return self.path / 'temp'
-
     def __attrs_post_init__(self):
-        with open(_get_storage_meta_path(self.path), 'r') as f:
+        with open(Storage._get_meta_path(self.path), 'r') as f:
             self.meta = json.load(f)
             self.hash_name = self.meta['hash_name']
 
     @staticmethod
-    def create(path: path_like, hash_name=DEFAULT_HASH) -> None:
-        path = Path(path)
+    def _get_meta_path(storage_path: AbsPath) -> AbsPath:
+        return storage_path / '.archivo-storage'
+
+    @staticmethod
+    def create(path: path_like, hash_name=DEFAULT_HASH) -> Storage:
+        path = ensure_abs(path)
         os.makedirs(path, exist_ok=False)
         metadata = {
             'created': now_to_text(),
             'hash_name': hash_name,
         }
-        write_json_file(metadata, _get_storage_meta_path(path))
+        write_json_file(metadata, Storage._get_meta_path(path))
+        return Storage(path)
 
 
     def has_file(self, file_spec: FileSpec) -> bool:
-        return self.get_file_path(file_spec).exists()
+        return self._get_storage_path(file_spec).exists()
 
 
-    def store(self, path: path_like) -> FileList:
-        path = Path(path)
-        tmp_dir = self.get_tmp_dir()
-        os.makedirs(tmp_dir, exist_ok=True)
-        with tempfile.TemporaryDirectory(dir=tmp_dir) as tmp_dir:
+    def store(self, src_path: path_like) -> FileOrDirSpec:
+        src_path = Path(src_path)
+        with tempfile.TemporaryDirectory(dir=self.path) as tmp_dir:
             tmp_dir = Path(tmp_dir)
+            tmp_path = _copy_into(src_path, tmp_dir)
 
-            _copy_into(path, tmp_dir)
+            spec = read_spec(tmp_path, self.hash_name)
 
-            file_list = make_file_list(tmp_dir, self.hash_name)
+            for rel_path, file_spec in iter_paths_and_specs(spec, dirs=False):
+                if self.has_file(file_spec):
+                    continue
 
-            moves = {
-                tmp_dir.joinpath(rel_path): self.get_file_path(file_spec)
-                for rel_path, file_spec in file_list.items()
-                if not self.has_file(file_spec)
-                }
+                tmp_path = tmp_dir / rel_path
+                dst_path = self._get_storage_path(file_spec)
+                os.makedirs(dst_path.parent, exist_ok=True)
+                os.rename(tmp_path, dst_path)
 
-            _move_files(moves)
+        return spec
 
-        return file_list
+    def _restore_metadata(self, spec, dst_path):
+        set_mode(dst_path, spec.mode)
+        set_mtime_ns(dst_path, spec.mtime_ns)
 
-    def _restore_to_tmp_dir(self, file_list, dst_dir):
-        for rel_path, file_spec in file_list.items():
-            src_path = self.get_file_path(file_spec)
-            dst_path = dst_dir.joinpath(rel_path)
-            os.makedirs(dst_path.parent, exist_ok=True)
-            shutil.copy2(src_path, dst_path)
-            _restore_stat_data(dst_path, file_spec)
+    def _restore_into(self, root_spec, dst_dir):
 
-            restored_spec = make_file_spec(dst_path, file_spec.hash_name)
-            if not file_spec.is_equivalent(restored_spec):
-                message = f'restored {restored_spec} but expected {file_spec}'
-                raise RestoreError(message)
+        root_dst_path = dst_dir / root_spec.name
 
+        # Create all the directories
+        for rel_path, dir_spec in iter_paths_and_specs(root_spec, files=False):
+            os.makedirs(dst_dir / rel_path, exist_ok=True)
 
-    def restore(self, file_list: FileList, dst_dir: path_like) -> None:
+        # Copy all the files
+        for rel_path, file_spec in iter_paths_and_specs(root_spec, dirs=False):
+            src_path = self._get_storage_path(file_spec)
+            shutil.copy2(src_path, dst_dir / rel_path)
+
+        # Restore metadata
+        for rel_path, spec in iter_paths_and_specs(root_spec):
+            self._restore_metadata(spec, dst_dir / rel_path)
+
+        # Check that results are correct
+        restored_spec = read_spec(root_dst_path, root_spec.hash_name)
+
+        if not restored_spec == root_spec:
+            raise RestoreError(
+                message='Restoration resulted in the wrong spec.',
+                desired=root_spec,
+                restored=restored_spec
+                )
+
+    def restore(self, spec: FileOrDirSpec, dst_dir: path_like) -> None:
         dst_dir = Path(dst_dir)
+        dst_path = dst_dir / spec.name
 
-        colliders = {p for p in file_list if dst_dir.joinpath(p).exists()}
-
-        for rel_path in colliders:
-            desired_spec = file_list[rel_path]
-            dst_path = dst_dir.joinpath(rel_path)
-            existing_spec = make_file_spec(dst_path, desired_spec.hash_name)
-            if not existing_spec.is_equivalent(desired_spec):
+        if dst_path.exists():
+            existing_spec = read_spec(dst_path, spec.hash_name)
+            if existing_spec != spec:
                 raise CollisionError(
-                    f'collision at {dst_path}: '
-                    f'existing {existing_spec} '
-                    f'but should place {desired_spec}'
+                    f'Another file or directory is on path {dst_path}',
+                    desired=spec,
+                    existing=existing_spec
                     )
 
-        to_restore = {k: v for k, v in file_list.items() if k not in colliders}
+            # All fine; no restore needed
+            return
 
-        tmp_dir = self.get_tmp_dir()
-        os.makedirs(tmp_dir, exist_ok=True)
-        with tempfile.TemporaryDirectory(dir=tmp_dir) as tmp_dir:
+        with tempfile.TemporaryDirectory(dir=self.path) as tmp_dir:
             tmp_dir = Path(tmp_dir)
-            self._restore_to_tmp_dir(to_restore, tmp_dir)
-
-            moves = {
-                tmp_dir.joinpath(rel_path): dst_dir.joinpath(rel_path)
-                for rel_path in to_restore
-            }
-
-            _move_files(moves)
+            tmp_path = tmp_dir / spec.name
+            self._restore_into(spec, tmp_dir)
+            os.rename(tmp_path, dst_path)
